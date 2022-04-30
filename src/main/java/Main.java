b5336ed0799e68d042358545ad841a5b80d657de
Alice <80988747+Licettte@users.noreply.github.com>
2022-04-30 23:58:26 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        FlightBuilder flightBuilder = new FlightBuilder();
-        List<Flight> flightList = flightBuilder.createFlights();
+      
+        List<Flight> flightList = FlightBuilder.createFlights();
 
 
         UtilityFlightFilter.selectFilter(flightList, TypeOfFilter.FILTERFIRST);
